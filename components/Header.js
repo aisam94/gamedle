70f9c660df70bac7b907819d40ef82a7903fc1dc
Aisamuddin <aisam0094@gmail.com>
2022-03-19 12:11:08 +0800

    edit header

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <h1 className="title">GAMEGECKO</h1>
+      <h1 className="title">GAMEDLE</h1>
     </header>
   );
 };
