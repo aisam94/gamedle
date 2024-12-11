fa8eddab5589377a955dc947240a35f9662aeb7f
Aisamuddin <aisam0094@gmail.com>
2024-12-11 18:56:42 +0800

    chore: tidy up index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,7 @@
-import React, { useState, useEffect, useContext } from "react";
+import React from "react";
 import Header from "../components/Header";
-import Grid from "../components/Grid";
-import Keyboard from "../components/Keyboard";
-import { MAX_WORD_LENGTH, MAX_TRIES } from "../constants/settings";
-import { answer, isWinningWord, isWordInList } from "../lib/words";
-import Alert from "../components/Alert";
 import useTheme from "../styles/theme";
 import GuessWordContainer from "../components/GuessWordContainer";
-
-let triesLeft = MAX_TRIES;
 
 const HomePage = () => {
 	const { theme } = useTheme();
