12ddc80ce6694e1ecc25d698c03d24e27844bcdf
Aisamuddin <aisam0094@gmail.com>
2024-12-11 18:55:12 +0800

    chore: tidy up code syntax

--- pages/_app.js
+++ pages/_app.js
@@ -6,26 +6,26 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-          <title>Gamedle</title>
-          <meta name="description" content="Gamedle" />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
+        <title>Gamedle</title>
+        <meta name="description" content="Gamedle" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <ThemeProvider>
         <Component {...pageProps} />
