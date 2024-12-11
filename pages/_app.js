dcb7f6a15a2e7d85905ef79b1a40a79d53077f9b
Aisamuddin <aisam0094@gmail.com>
2024-12-11 12:12:18 +0800

    feat: implement favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,35 @@
 import "../styles/styles.css";
 import { ThemeProvider } from "../styles/theme";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+          <title>Gamedle</title>
+          <meta name="description" content="Gamedle" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
